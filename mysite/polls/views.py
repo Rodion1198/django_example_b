626e8cc47de338143031265c1a4fdd340f196db5
Rodion1198 <rodka1198@gmail.com>
2020-12-13 22:28:35 +0200

    change method get_queryset()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,23 +4,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 
 # def index(request):
 #   return HttpResponse("Hello, world. You're at the polls index.")
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
@@ -45,19 +55,5 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
