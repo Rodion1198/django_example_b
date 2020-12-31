13eabb71b27e5549f8a2f12a294d4ca158230406
Rodion1198 <rodka1198@gmail.com>
2020-12-31 14:45:28 +0200

    added new form UserModelForm

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -1,8 +1,16 @@
 from django import forms
 
+from .models import User
+
 
 class ContactFrom(forms.Form):
     first_cathetus = forms.IntegerField(required=True,
                                         help_text=("enter positive numbers"))
     second_cathetus = forms.IntegerField(required=True,
                                          help_text=("enter positive numbers"))
+
+
+class UserModelForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'email']
