2462b67e199399e6cad28d3cd4cd6ae3973dffd6
Rodion1198 <rodka1198@gmail.com>
2021-01-08 17:40:56 +0200

    added custom  middleware

---
+++ mysite/polls/middleware.py
@@ -0,0 +1,22 @@
+import datetime
+
+from .models import Logg
+
+
+def LogMiddleware(get_response):
+    def middleware(request):
+
+        request.current_time = datetime.datetime.now()
+        # a = [i for i in str(request.path).split('/')]
+        logg = Logg(path=request.path,
+                    method=request.method,
+                    timestamp=request.current_time)
+        if logg.path == "/admin/":
+            return logg.delete()
+        else:
+            logg.save()
+        response = get_response(request)
+
+        return response
+
+    return middleware
