5c3fee516a8bc59985f970286e737b378a58ea03
Rodion1198 <rodka1198@gmail.com>
2021-01-19 22:32:25 +0200

    added some settings

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -13,6 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+import debug_toolbar
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import RedirectView
@@ -24,6 +29,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('user/create', user_create, name='user_create'),
-    path('user/edit/<int:pk>/', user_edit, name='user_edit')
+    path('user/edit/<int:pk>/', user_edit, name='user_edit'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-]
+if settings.DEBUG:
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+        path('silk/', include('silk.urls', namespace='silk')),
+    ]
