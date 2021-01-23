0c6211b67b87f3016c36c4099fb95bfc783120c8
Rodion1198 <rodka1198@gmail.com>
2021-01-23 20:52:02 +0200

    added TestForm for send email

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -14,3 +14,9 @@ class UserModelForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['id', 'first_name', 'last_name', 'email']
+
+
+class TestForm(forms.Form):
+    from_email = forms.EmailField(required=True)
+    message = forms.CharField(required=True)
+    time_to_send = forms.DateTimeField()
