5fed74e2a09240713afadb3e5f9180dc262f80f2
Rodion1198 <rodka1198@gmail.com>
2021-01-28 16:49:07 +0200

    implemented parsing func to get quotes

--- mysite/polls/tasks.py
+++ mysite/polls/tasks.py
@@ -1,23 +1,62 @@
+from bs4 import BeautifulSoup
+
 from celery import shared_task
 
 from django.core.mail import send_mail as my_send_mail
 
+from polls.models import Quot, QuoteAuthor
 
-@shared_task
-def add(x, y):
-    return x + y
-
-
-@shared_task
-def mul(x, y):
-    return x * y
+import requests
 
 
-@shared_task
-def xsum(numbers):
-    return sum(numbers)
+# @shared_task
+# def send_email(from_email, message, time_to_send):
+#     my_send_mail(from_email, message, time_to_send, ['admin@example.com'])
 
 
 @shared_task
-def send_email(from_email, message, time_to_send):
-    my_send_mail(from_email, message, time_to_send, ['admin@example.com'])
+def parse_quote():
+    page = 1
+    last_page = False
+    already_saved_quotes = 1
+    LINK = 'https://quotes.toscrape.com'
+
+    while not last_page:
+        url = f'https://quotes.toscrape.com/page/{page}'
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        quotes = soup.findAll('div', {'class': 'quote'})
+
+        for item in quotes:
+            if already_saved_quotes > 5:
+                break
+            else:
+                title = item.select('.text')[0].contents[0]
+                if Quot.objects.filter(title=title):
+                    pass
+                else:
+                    author = item.select('.author')[0].contents[0]
+                    if QuoteAuthor.objects.filter(author=author):
+                        saved_author = QuoteAuthor.objects.get(author=author)
+                    else:
+                        link_author = LINK + item.find_all('a')[0].get('href')
+                        r = requests.get(link_author)
+                        soup = BeautifulSoup(r.content, 'html.parser')
+                        date_of_birth = soup.find('span', {'class': 'author-born-date'}).contents[0]
+                        born_in = soup.find('span', {'class': 'author-born-location'}).contents[0]
+                        description = soup.find('div', {'class': 'author-description'}).contents[0]
+                        saved_author: QuoteAuthor = QuoteAuthor(author=author, date_of_birth=date_of_birth,
+                                                                born_in=born_in, description=description)
+                        saved_author.save()
+                    quote_record: Quot = Quot(title=title, author=saved_author)
+                    quote_record.save()
+                    already_saved_quotes += 1
+        if already_saved_quotes < 5:
+            if soup.find('li', {'class': 'next'}) is None:
+                last_page = True
+            else:
+                page += 1
+        else:
+            break
+        if last_page is True:
+            my_send_mail('Quotes is over!', 'All quotes added!', 'rodion@test.com', ['admin@example.com'])
